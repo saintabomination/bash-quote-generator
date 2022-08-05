303fec38b75c3c20ecee640fe17e121915c5fb61
saintabomination <saint.abomination@protonmail.com>
2022-08-05 23:09:21 +0200

    Adding quote symbols to the printed quote

--- quote-gen.sh
+++ quote-gen.sh
@@ -3,6 +3,9 @@
 SELECTED_QUOTE=$(shuf -n 1 quotes.txt)
 IFS='_' read -ra ADDR <<< "$SELECTED_QUOTE"
 
+ADDR[0]=$(sed 's/^/„/' <<< ${ADDR[0]})
+ADDR[0]=$(sed 's/$/“/' <<< ${ADDR[0]})
+
 for i in "${ADDR[@]}"; do
   echo $i
 done
