5f5435248a7cc6b8ed1d3abcd39150892c7ee75c
saintabomination <saint.abomination@protonmail.com>
2022-08-05 23:20:51 +0200

    Better quote formatting

--- quote-gen.sh
+++ quote-gen.sh
@@ -5,7 +5,9 @@ IFS='_' read -ra ADDR <<< "$SELECTED_QUOTE"
 
 ADDR[0]=$(sed 's/^/„/' <<< ${ADDR[0]})
 ADDR[0]=$(sed 's/$/“/' <<< ${ADDR[0]})
+ADDR[0]="${ADDR[0]}\n"
+ADDR[1]=$(sed 's/^/- /' <<< ${ADDR[1]})
 
 for i in "${ADDR[@]}"; do
-  echo $i
+  echo -e $i
 done
